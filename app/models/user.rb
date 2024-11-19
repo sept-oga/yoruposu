1b72b9db381afe417f042bf5c6cbc487e9639600
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-11-19 09:00:23 +0000

    [11-19] PF

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  ## postsテーブルを作成したら
+  ## has_many :posts, dependent: :destroy
   
   validates :name, presence: true
+
+  has_one_attached :profile_image
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
 end
