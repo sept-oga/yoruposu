601402fe6590ebaf2e1dd2ff8329c4530906be38
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-11-19 12:23:04 +0000

    [11-19] PF

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post)
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :image, :body)
   end
 end
---
+++ db/migrate/20241119115645_ad_user_id_to_posts.rb
@@ -0,0 +1,4 @@
+class AdUserIdToPosts < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
