90164f1dcbabad0d3cd6e2f0c3f5da643d58c049
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-11-20 13:10:49 +0000

    [11-20] PF ゲストログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -25,5 +26,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.guest_user?
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   get 'about', to: 'homes#about', as: 'about'
   resources :users, only: [:show, :edit, :update, :destroy]
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  # ゲストログイン機能
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
