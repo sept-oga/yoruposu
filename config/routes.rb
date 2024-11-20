7145efd58a6a9d763ee5df608352c2b988b71624
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-11-20 02:12:18 +0000

    [11-20] PF

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get 'about', to: 'homes#about', as: 'about'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:new, :create, :index, :show, :edit, :destroy]
 end
