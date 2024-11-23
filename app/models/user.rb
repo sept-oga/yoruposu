b529952fb31890abd5a7aa7d560ccd2ad8c04a38
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-11-23 11:50:16 +0000

    [11-22] PF テスト修正中

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  def get_profile_image(width, height)
+  def get_profile_image
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no-image.jpeg')
+      file_path = Rails.root.join('app/javascript/images/no-image.jpeg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [100, 100]).processed
   end
 
   GUEST_USER_EMAIL = "guest@example.com"
