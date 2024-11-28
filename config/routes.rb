d2e4dad5c5b5055f66f568b1a3a0e48441b2cae8
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-11-28 13:11:32 +0000

    [11-28] PF admin 各種設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
