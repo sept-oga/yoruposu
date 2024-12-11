3536b8d059621aeff8d1d370a8c0a5871157b265
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-12-11 09:03:47 +0000

    [12-11] PF Test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_27_113602) do
+ActiveRecord::Schema.define(version: 2024_12_11_085120) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +67,11 @@ ActiveRecord::Schema.define(version: 2024_11_27_113602) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
