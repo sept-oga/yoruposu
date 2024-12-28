aa71c839f27253eaadb3aee1f8aeda8c464eb8a2
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-12-28 02:12:43 +0000

    [12-28] PF address check

--- app/javascript/packs/jpostal.js
+++ app/javascript/packs/jpostal.js
@@ -2,7 +2,7 @@ function jpostal() {
   $('#zipcode').jpostal({
     postcode : ['#zipcode'],
     address : {
-      '#post_image_address': '%3%4%5'
+      '#group_post_address': '%3%4%5'
     }
   });
 }
